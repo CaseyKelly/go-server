778d42c127b9b76a70c238c96a35a82a3ba5a229
Casey Kelly <casey.kelly@colorado.edu>
2016-06-16 14:14:54 -0400

    add query function to main.go

--- main.go
+++ main.go
@@ -19,3 +19,20 @@ type weatherData struct {
 		Kelvin float64 `json:"temp"`
 	} `json:"main"`
 }
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=af50b3aaac845b0bd2cda806876dfca9&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
